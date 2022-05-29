7256a750071fabf8dd1d5515e00e4cd7b258588c
Luiza Gusmão <luiza.gusmao@dibibee.com>
2022-05-29 17:22:46 -0300

    adicionando plugin do xpath e cenário 'should update an account'

--- cypress/integration/functional.spec.js
+++ cypress/integration/functional.spec.js
@@ -11,9 +11,25 @@ describe('Should test at a funcional level', () => {
 
     it('Should create an account', () => {
         cy.get('[data-test="menu-settings"]').click()
+        cy.get('[href="/reset"]').click()
+        cy.get('[data-test="menu-settings"]').click()
         cy.get('[href="/contas"]').click()
-        cy.get('[data-test="nome"]').type('Conta teste')
+        cy.get('[data-test="nome"]').type('Conta teste 2')
         cy.get('.btn').click()
         cy.get('.toast-success > .toast-message').should('contain', 'Conta inserida com sucesso!')
+    })
+
+    it('Should update an account', () => {
+        cy.get('[data-test="menu-settings"]').click()
+        cy.get('[href="/contas"]').click()
+        //o locator "edit" é frágil por estar determinando a posição na página
+        // cy.get(':nth-child(7) > :nth-child(2) > :nth-child(1) > .far')
+        cy.xpath("//table//td[contains(., 'Conta teste 2')]/..//i[@class='far fa-edit']").click()
+        cy.get('[data-test="nome"]')
+            .clear()
+            .type('Conta 2 alterada')
+        cy.get('.btn').click()
+        cy.get('.toast-success > .toast-message').should('contain', 'Conta atualizada com sucesso')
+
     })
 })
